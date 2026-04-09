8ec9fe3f1470da9e324e005d2f309c89494beff4
tu <vutu201130@gmail.com>
2026-04-09 14:57:16 +0700

    remove head_first

--- zoology/mixers/delta_net_multiscale.py
+++ zoology/mixers/delta_net_multiscale.py
@@ -240,7 +240,6 @@ class DeltaNetMultiScale(nn.Module):
                 initial_state=recurrent_state,
                 output_final_state=use_cache,
                 cu_seqlens=cu_seqlens,
-                head_first=False,
                 use_qk_l2norm_in_kernel=True,
             )
         elif mode == 'fused_recurrent':
@@ -249,7 +248,6 @@ class DeltaNetMultiScale(nn.Module):
                 initial_state=recurrent_state,
                 output_final_state=use_cache,
                 cu_seqlens=cu_seqlens,
-                head_first=False,
                 use_qk_l2norm_in_kernel=True,
             )
         else:
